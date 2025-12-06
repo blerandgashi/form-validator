1f2a497a01b3f3853bf91332709743cbe7fd62e5
Blerand Gashi <blerandgashi15@gmail.com>
2025-12-06 20:52:34 +0100

    finished form validator project

--- script.js
+++ script.js
@@ -9,32 +9,45 @@ let password2 = document.querySelector("#password2");
 const showErrorText = document.querySelector(".form-control");
 
 form.addEventListener("submit", function(e){
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   e.preventDefault();
-  
+
+  // Username part
   if (userName.value === "") {
     showError(userName, "Username is required!")
+  }else if (userName.value.length < 7) {
+    showError(userName, "Username is too short!")
+  }else if(userName.value.length > 15){
+    showError(userName, "Username is too long!")
   }else{
-    showSuccess(userName);
+    showSuccess(userName)
   }
 
+  // Email part
   if (email.value === "") {
     showError(email, "Email is required!")
+  }else if (!emailRegex.test(email.value)) {
+    showError(email, "Email is not correct!");
   }else{
-    showSuccess(email)
+    showSuccess(email);
   }
 
+  // Password part
   if (password.value === "") {
     showError(password, "Password is required!")
+  }else if (password.value.length < 7) {
+    showError(password, "Password is too short!")
   }else{
     showSuccess(password)
   }
 
   if (password2.value === "") {
     showError(password2, "Confirm password!")
+  }else if (password.value !== password2.value) {
+    showError(password2, "Passwords don't match!")
   }else{
-    showError(password2)
+    showSuccess(password2)
   }
-  
 })
 
 function showError(input, message){
